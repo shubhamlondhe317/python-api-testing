3b6ecd9e98dc25af0acdafb623a9d9feea2be5d3
shubhamlondhe317 <shubham.londhe317@gmail.com>
2023-03-09 13:15:08 +0530

    new changes

--- tests/test_technical_details.py
+++ tests/test_technical_details.py
@@ -12,6 +12,19 @@ print(student_technical_details)
     student_technical_details,
     ids=[x["id"] for x in student_technical_details],
 )
+def test_get_technical_student_details(start_executions, test_params, base_url):
+    print(test_params)
+    response = start_executions.get(base_url + "/api/technicalskills")
+    print(response.text)
+    assert response.status_code == 200
+
+
+@pytest.mark.SVT
+@pytest.mark.parametrize(
+    "test_params",
+    student_technical_details,
+    ids=[x["id"] for x in student_technical_details],
+)
 def test_technical_student_details(start_executions, test_params, base_url):
     print(test_params)
     response = start_executions.post(base_url + "/api/technicalskills", test_params)
