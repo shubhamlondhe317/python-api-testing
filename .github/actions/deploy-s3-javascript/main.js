8c1229d6713f75433f3add0612f8b7930fb4af59
shubhamlondhe317 <shubham.londhe317@gmail.com>
2023-05-16 11:27:29 +0530

    composite

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,14 @@ const exec = require('@actions/exec');
 
 
 function run(){
+
+    const bucket = core.getInput('bucket', {required:true});
+    const bucketRegion = core.getInput('bucket_region', {required:true});
+    const distFolder = core.getInput('dist_folder', {required:true});
+
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${}distFolder ${s3Uri} --region ${bucketRegion}`);
+
     core.notice('custom javascript actions');
 
 }
